e559fddd3afe3795ec3782a370c753c11557f779
StevenLi-Projects <stevenli8152@gmail.com>
2025-11-12 12:35:31 -0500

    Update SerialMonitor.py

--- SerialMonitor.py
+++ SerialMonitor.py
@@ -1,12 +1,13 @@
 import serial
 import time
-serial_port = serial.Serial('COM3',9600, timeout = 1)
+import serial.tools.list_ports
+
+serial_port = serial.Serial('COM5', 9600, timeout=5)
 time.sleep(3)
 file_path = "data.txt"
 with open(file_path, "a") as file:
     while True:
         if serial_port.in_waiting > 0:
-            # Read one line from the serial port
-            line = serial_port.readline().decode('ascii', errors='ignore').strip()    
-            f.write(line + '\n')
-            f.flush()        
+            line = serial_port.readline().decode('ascii', errors='ignore').strip()
+            file.write(line + '\n')
+            file.flush()
